d05c28b9cdbab74c24b127f044a3ac84b9909b12
Christo sajy <146114187+christosajy@users.noreply.github.com>
2024-01-07 11:14:13 +0530

    Update settings.py

--- AdventProject/settings.py
+++ AdventProject/settings.py
@@ -132,9 +132,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'flybuscargo@gmail.com'
-EMAIL_HOST_PASSWORD = 'suyucdgycqigkace'
-DEFAULT_FROM_EMAIL = 'FLYBUS LOGISTICS <flybuscargo@gmail.com>'
+EMAIL_HOST_USER = ''        # ADD HOST EMAIL ADDRESS
+EMAIL_HOST_PASSWORD = ''    # ADD HOST APP GENERATED PASSWORD
+# DEFAULT_FROM_EMAIL = 'FLYBUS LOGISTICS <ADD HOST EMAIL ADDRESS>'
 
 # Login Settings
 LOGIN_URL = 'userform'
